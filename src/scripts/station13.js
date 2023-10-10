fab840b6687c5a542ecf2c05eba35b8f64855a2a
yuto99999 <myuto2003@gmail.com>
2023-10-10 16:45:27 +0900

    10/10クリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const button = document.getElementById("check");
+  if (button.checked) {
+    document.getElementById("text").style.backgroundColor = "red";
+  } else {
+    document.getElementById("text").style.backgroundColor = "#000000";
+  }
 }
---
+++ src/scripts/station14.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById("btn");
+function clicked() {
+  alert("ECMAScript");
+}
